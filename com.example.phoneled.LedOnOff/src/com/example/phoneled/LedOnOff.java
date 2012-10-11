7544ba27ac9021a8f5a3593ae47d63cd474ad9ed
Tom Xue <tomxue0126@gmail.com>
2012-10-11 22:17:18 +0800

    cancelAll() applied

--- com.example.phoneled.LedOnOff/src/com/example/phoneled/LedOnOff.java
+++ com.example.phoneled.LedOnOff/src/com/example/phoneled/LedOnOff.java
@@ -29,19 +29,13 @@ public class LedOnOff extends Activity {
 				// notification.icon = R.drawable.someicon;  // there should be spec for the icon
 				if (isChecked) {
 					// 这里是颜色，我们可以尝试改变，理论上0xFF0000是红色，0x00FF00是绿色
-					notification.ledARGB = 0xFFFFFFFF;
-					notification.ledOnMS = 1000;
-					notification.ledOffMS = 100;
-					notification.flags = Notification.FLAG_SHOW_LIGHTS;
-					nm.notify(ID_LED, notification);
-					// nm.cancel(ID_LED);
-				} else {
-					notification.ledARGB = 0x00FF;
+					notification.ledARGB = 0xFF0000;
 					notification.ledOnMS = 2000;
 					notification.ledOffMS = 100;
-					notification.flags = Notification.FLAG_SHOW_LIGHTS;
+					notification.flags |= Notification.FLAG_SHOW_LIGHTS;
 					nm.notify(ID_LED, notification);
-					// nm.cancel(ID_LED);
+				} else {
+					nm.cancelAll();
 				}
 			}
 		});
